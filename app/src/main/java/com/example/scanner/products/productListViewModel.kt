842f60c111c081022280cf55b1c3c1b0f133d9c0
Lina <144618594+rinrina01@users.noreply.github.com>
2025-11-05 09:33:07 +0100

    feat/ adds product list states

--- app/src/main/java/com/example/scanner/products/productListViewModel.kt
+++ app/src/main/java/com/example/scanner/products/productListViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.scanner.products
 
+import com.example.scanner.common.ApiError
+import kotlinx.coroutines.flow.MutableStateFlow
+
+sealed class ProductListUiState {
+    /* children classes are all possible states depending on :
+    - Waiting for response :
+        Loading
+    - received API response :
+        Success (200)
+        Error (400, 300, 500)
+     */
+    data object Loading : ProductListUiState() // mark as object when no attributes
+    data class Success(val products: List<Product>) : ProductListUiState()
+    data class Failure(val message: String, val error: ApiError) : ProductListUiState()
+}
+
+fun LoadProduct() {
+    val productFlow = MutableStateFlow<ProductListUiState>(ProductListUiState.Loading)
+}
