4697356ff7f55fe42142e0cd27b519b13366c482
RomualdBR <rbrisacier@gmail.com>
2025-11-05 16:20:04 +0100

    add liste database for many products

--- app/src/main/java/com/example/scanner/barcode/barcodeActivity.kt
+++ app/src/main/java/com/example/scanner/barcode/barcodeActivity.kt
@@ -1,27 +1,21 @@
 package com.example.scanner.barcode
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.scanner.products.ProductListActivity
-import com.example.scanner.R
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.scanner.common.ApiCall
 import com.example.scanner.common.ApiResponse
-import com.example.scanner.products.ProductListScreen
+import com.example.scanner.products.ProductViewModel
 import com.example.scanner.ui.theme.ScannerTheme
-import com.google.android.datatransport.runtime.dagger.Component
 
 class barcodeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +32,7 @@ class barcodeActivity : ComponentActivity() {
 
 @Composable
 fun Barcode() {
+    var Productvm: ProductViewModel = viewModel()
     var reading by remember { mutableStateOf(true) }
     val context = LocalContext.current
     BarcodeScannerScreen(
@@ -48,10 +43,8 @@ fun Barcode() {
 
                 println((product as ApiResponse.Success).product.product_name)
 
-                /*@TODO
-                * adding to database and mutableStateFlow
-                 */
-
+                Productvm.createProduct(product.product)
+                println(Productvm.getProducts())
 
                 (context as Activity?)?.finish();
             }
